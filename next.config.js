ab6cd3e154ab07f1bc80055727da8864ad526a58
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-01 02:11:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NFT_MARKET_CONTRACT_ADDRESS:"0x770fA3FBE1af3B4675611C1dAC2CeB6C64A877fb",
-    NFT_CONTRACT_ADDRESS:"0x21f9669CE0705Dfd91ac653749ef0ED4d9E3C912",
+    NFT_MARKET_CONTRACT_ADDRESS:"",
+    NFT_CONTRACT_ADDRESS:"",
     CHAIN_ID:2330
   },
   reactStrictMode: true,
